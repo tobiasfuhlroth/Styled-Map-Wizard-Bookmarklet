e630ebcd4478f42e9d62ccdea859e2a48283fd89
Tobias Fuhlroth <tobias.fuhlroth@notch-interactive.ch>
2012-03-20 16:01:24 +0100

    file structure drafts

---
+++ configurator.js
@@ -0,0 +1,30 @@
+/**
+ * Configurator
+ */
+
+
+define(['text!./configurator.html'], function (template) {
+
+    "use strict";
+
+    var Configurator = new Class({
+
+        template: template,
+
+        initialize: function () {
+            this.build();
+        },
+
+        build: function () {
+            this.container = new Element('div', {
+                'id': 'smw-configurator-container',
+                'html': this.template
+            });
+            this.container.inject($(document.body));
+        }
+
+    });
+
+    return Configurator;
+
+});
