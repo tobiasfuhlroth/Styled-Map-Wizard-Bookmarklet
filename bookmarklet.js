1977bea6a082b412a5b2d3d8dedfc9fb09d9f645
Tobias Fuhlroth <tobias.fuhlroth@notch-interactive.ch>
2012-03-20 13:47:23 +0100

    bookmarklet and main file for require js

---
+++ bookmarklet.js
@@ -0,0 +1,7 @@
+javascript: (function () {
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'http://tobiasfuhlroth.github.com/Styled-Map-Wizard-Bookmarklet/require.js';
+    script.setAttribute('data-main', 'http://tobiasfuhlroth.github.com/Styled-Map-Wizard-Bookmarklet/main');
+    document.body.appendChild(script);
+}());
---
+++ main.js
@@ -0,0 +1,15 @@
+/**
+ * Styled Map Wizard Bookmarklet
+ * @author Tobias Fuhlroth - Notch Interactive GmbH
+ */
+
+
+console.log('Test');
+
+require([], function () {
+
+    "use strict";
+
+
+
+});
