dee8623eb14c37f977c22ee944bdd4eaf1993937
Tobias Fuhlroth <tobias.fuhlroth@notch-interactive.ch>
2012-03-20 15:38:01 +0100

    added mootools and text-plugin

--- main.js
+++ main.js
@@ -5,11 +5,30 @@
 
 
 console.log('Test');
+require.config({
+    paths: {
+        'text': './text',
+        'mootools': './mootools-core-1.4.5-full-nocompat-yc.js'
+    }
+});
 
-require([], function () {
+
+require(['mootools', 'text'], function () {
 
     "use strict";
 
-
+    window.addEvent('domready', function () {
+        new Element('div', {
+            'styles': {
+                'width': 200,
+                'height': 100,
+                'background-color': 'red',
+                'position': 'absolute',
+                'left': 100,
+                'top': 100,
+                'z-index': 10000
+            }
+        }).inject($(document.body));
+    });
 
 });
